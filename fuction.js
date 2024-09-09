c2dae471992f7a1a0c959e58005370c629af2440
llipe_20 <silvahlipe@gmail.com>
2024-09-09 01:28:56 -0300

    vai teia

---
+++ fuction.js
@@ -0,0 +1,7 @@
+var title = document.getElementById("cantada")
+var buttons = document.getElementById("container")
+
+const rederizar = () => {
+    title.innerText = "- Doutora, estou mau, hackearam meu coração, podemos marcar a sessão de recuperação?"
+    buttons.classList.remove("hidden")
+}
