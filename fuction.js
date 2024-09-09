bf0582e4aa5a31858b5ab329735f70a0189e0983
llipe_20 <silvahlipe@gmail.com>
2024-09-09 17:26:35 -0300

    2

--- fuction.js
+++
@@ -1,7 +0,0 @@
-var title = document.getElementById("cantada")
-var buttons = document.getElementById("container")
-
-const rederizar = () => {
-    title.innerText = "- Doutora, estou mau, hackearam meu coração, podemos marcar a sessão de recuperação?"
-    buttons.classList.remove("hidden")
-}
